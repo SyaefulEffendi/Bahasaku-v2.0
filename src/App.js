299c45b7f45b9aa5da8db0df817f9d06f26fa025
SyaefulEffendi <mohsyaefuleffendi@gmail.com>
2026-03-12 10:13:35 +0700

    pembuatan endpoint/page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from './pages/dashboard';
+import Login from './pages/login';
+import Register from './pages/register';
+import Aplikasi from './pages/aplikasi';
+import TentangKami from './pages/tentang-kami';
+import Kontak from './pages/kontak';
+import TextToVideo from './pages/text-to-video';
+import VideoToText from './pages/video-to-text';
+import DashboardAdmin from './pages/dashboardAdmin';
+import Profile from './pages/profile';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/aplikasi" element={<Aplikasi />} />
+          <Route path="/tentang-kami" element={<TentangKami />} />
+          <Route path="/kontak" element={<Kontak />} />
+          <Route path="/text-to-video" element={<TextToVideo />} />
+          <Route path="/video-to-text" element={<VideoToText />} />
+          <Route path="/dashboard-admin" element={<DashboardAdmin />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
