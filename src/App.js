dfdaa0f4eb7a2e2a34a6d8b3f38a413e8e0e1927
SyaefulEffendi <mohsyaefuleffendi@gmail.com>
2026-04-14 01:59:27 +0700

    penambahan AuthProvider

--- src/App.js
+++ src/App.js
@@ -11,26 +11,28 @@ import TextToVideo from './pages/text-to-video';
 import VideoToText from './pages/video-to-text';
 import DashboardAdmin from './pages/dashboardAdmin';
 import Profile from './pages/profile';
-
+import { AuthProvider } from './context'; 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/aplikasi" element={<Aplikasi />} />
-          <Route path="/tentang-kami" element={<TentangKami />} />
-          <Route path="/kontak" element={<Kontak />} />
-          <Route path="/text-to-video" element={<TextToVideo />} />
-          <Route path="/video-to-text" element={<VideoToText />} />
-          <Route path="/dashboard-admin" element={<DashboardAdmin />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/aplikasi" element={<Aplikasi />} />
+            <Route path="/tentang-kami" element={<TentangKami />} />
+            <Route path="/kontak" element={<Kontak />} />
+            <Route path="/text-to-video" element={<TextToVideo />} />
+            <Route path="/video-to-text" element={<VideoToText />} />
+            <Route path="/dashboard-admin" element={<DashboardAdmin />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
